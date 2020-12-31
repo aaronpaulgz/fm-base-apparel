b1151a02facb8e717fd628d5d8c86ad7b4606104
Aaron Gonzalez <aarong.1503@gmail.com>
2020-12-31 09:12:35 -0500

    Desktop ready

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ $errTxt = document.getElementById("errTxt");
 const validateTxt = () => {
   if ($input.value.includes("@")) {
     console.log("Message sent");
+    // $error.style.visibility = "visible";
+    $errTxt.style.color = "#00B59A";
+    $errTxt.innerHTML = "Email sent";
   } else {
     $input.style.border = "solid 2px hsl(0, 93%, 68%)";
     $error.style.visibility = "visible";
+    $errTxt.style.color = "";
     $errTxt.innerHTML = "Please provide a valid mail";
   }
 };
